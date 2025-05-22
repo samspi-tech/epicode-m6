f5394c52beef02edf44ec074e286121c151a6a27
Andrea <andrew.ryan.r.a@gmail.com>
2025-05-22 22:57:53 +0200

    Imported cors in entry point

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
+const cors = require('cors');
 const express = require('express');
 require('dotenv').config();
+
 const PORT = 9099;
 const startServer = require('./config/db');
 const usersRoute = require('./routes/user.route');
-
 const server = express();
-server.use(express.json());
 
+server.use(cors());
+server.options('*', cors());
+
+server.use(express.json());
 server.use('/', usersRoute);
 
 startServer(PORT, server);
